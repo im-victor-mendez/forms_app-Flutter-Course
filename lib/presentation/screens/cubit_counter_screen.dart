a18bf693a73440b6945a22c6a4aeca26c1a228c6
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-12-21 10:13:47 -0600

    ci: Added counter state data into cubit_counter_screen

--- lib/presentation/screens/cubit_counter_screen.dart
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../blocs/counter_cubit/counter_cubit.dart';
 
 class CubitCounterScreen extends StatelessWidget {
   const CubitCounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: const _View(),
+    );
+  }
+}
+
+class _View extends StatelessWidget {
+  const _View();
+
+  @override
+  Widget build(BuildContext context) {
+    // Way 2 to print state data
+    final counterState = context.watch<CounterCubit>().state;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cubit Counter'),
+        title: Text('Cubit Counter: ${counterState.transactionCount}'),
         actions: [
           IconButton(
             onPressed: () {},
@@ -15,8 +33,14 @@ class CubitCounterScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Counter Value xxx'),
+      body: Center(
+        // Way 1 to print state data
+        child: BlocBuilder<CounterCubit, CounterState>(
+          buildWhen: (previous, current) => current.counter != previous.counter,
+          builder: (context, state) {
+            return Text('Counter Value ${state.counter}');
+          },
+        ),
       ),
       floatingActionButton: Wrap(
         alignment: WrapAlignment.end,
