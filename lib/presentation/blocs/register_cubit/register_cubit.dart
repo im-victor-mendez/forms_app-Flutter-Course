9ae582bb1d0a2170a7492dc193730bfb4038158d
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-12-22 09:44:07 -0600

    ci: Implemented username input into register_cubit

--- lib/presentation/blocs/register_cubit/register_cubit.dart
+++ lib/presentation/blocs/register_cubit/register_cubit.dart
@@ -1,14 +1,27 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
+
+import '../../../infrastructure/inputs/inputs.dart';
 
 part 'register_state.dart';
 
 class RegisterCubit extends Cubit<RegisterState> {
   RegisterCubit() : super(const RegisterState());
 
-  void usernameChanged(String value) => emit(state.copyWith(username: value));
+  void usernameChanged(String value) {
+    final username = Username.dirty(value);
+
+    emit(state.copyWith(
+      username: username,
+      isValid: Formz.validate([username]),
+    ));
+  }
+
   void emailChanged(String value) => emit(state.copyWith(email: value));
+
   void passwordChanged(String value) => emit(state.copyWith(password: value));
+
   void formStatusChanged(FormStatus value) =>
       emit(state.copyWith(formStatus: value));
 }
--- lib/presentation/blocs/register_cubit/register_state.dart
+++ lib/presentation/blocs/register_cubit/register_state.dart
@@ -3,31 +3,35 @@ part of 'register_cubit.dart';
 enum FormStatus { invalid, valid, validating, posting }
 
 class RegisterState extends Equatable {
-  final String username;
+  final Username username;
   final String email;
   final String password;
   final FormStatus formStatus;
+  final bool isValid;
 
   const RegisterState({
-    this.username = '',
+    this.username = const Username.pure(),
     this.email = '',
     this.password = '',
     this.formStatus = FormStatus.invalid,
+    this.isValid = false,
   });
 
   @override
-  List<Object> get props => [username, email, password, formStatus];
+  List<Object> get props => [username, email, password, formStatus, isValid];
 
   RegisterState copyWith({
-    String? username,
+    Username? username,
     String? email,
     String? password,
     FormStatus? formStatus,
+    bool? isValid,
   }) =>
       RegisterState(
         username: username ?? this.username,
         email: email ?? this.email,
         password: password ?? this.password,
         formStatus: formStatus ?? this.formStatus,
+        isValid: isValid ?? this.isValid,
       );
 }
